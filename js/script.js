f39cb859811d21bd2acfb7010a4bf1e6b8a51490
chaneu <aluno.etec@etecEmbu.com.br>
2022-08-25 21:54:52 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+console.log('hello world'); //criar um comentario de linha
+/*
+  criar um comentario em bloco
+*/
+
+let n1 = 5;
+const n2 = 10;
+let result = n1+n2;
+
+console.log(n2);
+console.log(n1);
+console.log('Quantidades de cookies aceitos :', result);
+
+
+
+
+
+alert ('Gostaria de aceitar ' + result +  ' cookies ?');
+
+
